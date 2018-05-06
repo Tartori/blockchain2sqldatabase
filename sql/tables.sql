7e54b704a5b7cbdd936291f50b7cb9345a3a09a4
Julian <julianstampfli4@gmail.com>
2018-05-06 19:12:19 +0200

    first draft of sql tables

---
+++ sql/tables.sql
@@ -0,0 +1,126 @@
+CREATE DATABASE `testnet3` /*!40100 COLLATE 'latin1_swedish_ci' */;
+
+USE `testnet3`;
+
+/* CREATE USER 'testnet3'@'localhost' IDENTIFIED BY 'testnet3'; */
+GRANT SELECT, EXECUTE, SHOW VIEW, ALTER, ALTER ROUTINE, CREATE, CREATE ROUTINE, CREATE TEMPORARY TABLES, CREATE VIEW, DELETE, DROP, EVENT, INDEX, INSERT, REFERENCES, TRIGGER, UPDATE, LOCK TABLES  ON `testnet3`.* TO 'testnet3'@'localhost' WITH GRANT OPTION;
+
+
+CREATE TABLE `block` (
+	`block_hash` VARCHAR(64) NOT NULL,
+	`height` INT NOT NULL,
+	`size` INT NOT NULL,
+	`blockweight` INT NOT NULL,
+	`version` INT NOT NULL,
+	`merkleroot` VARCHAR(64) NOT NULL,
+	`time` DATETIME NOT NULL,
+	`mediantime` DATETIME NOT NULL,
+	`difficulty` FLOAT NOT NULL,
+	`chainwork` VARCHAR(64) NOT NULL,
+	`nonce` INT NOT NULL,
+	PRIMARY KEY (`block_hash`),
+	INDEX `height` (`height`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `transaction` (
+	`txid` VARCHAR(64) NOT NULL,
+	`block_hash` VARCHAR(64) NOT NULL,
+	`txhash` VARCHAR(64) NULL DEFAULT NULL,
+	`version` INT(11) NULL DEFAULT NULL,
+	`size` INT(11) NULL DEFAULT NULL,
+	`locktime` DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (`txid`),
+	INDEX `txhash` (`txhash`),
+	CONSTRAINT `block_transaction` FOREIGN KEY (`block_hash`) REFERENCES `block` (`block_hash`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+ENGINE=InnoDB
+;
+
+CREATE TABLE `vout` (
+	`txid` VARCHAR(64) NOT NULL,
+	`voutid` INT(11) NOT NULL,
+	`value` DECIMAL(65,30) NULL DEFAULT '0',
+	`type` VARCHAR(50) NULL DEFAULT NULL,
+	INDEX `transaction_vout` (`txid`),
+	PRIMARY KEY (`txid`, `voutid`),
+	CONSTRAINT `transaction_vout` FOREIGN KEY (`txid`) REFERENCES `transaction` (`txid`) ON UPDATE CASCADE ON DELETE CASCADE
+	)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+CREATE TABLE `op_code` (
+	`code` INT NOT NULL,
+	`name` VARCHAR(50) NULL,
+	PRIMARY KEY (`code`),
+	INDEX `name` (`name`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+CREATE TABLE `output_script_instruction` (
+	`txid` VARCHAR(64) NOT NULL,
+	`voutid` INT(11) NOT NULL,
+	`line` INT(11) NOT NULL,
+	`opcode` INT(11) NULL DEFAULT NULL,
+	`value` TEXT NULL DEFAULT NULL,
+	PRIMARY KEY (`txid`, `voutid`, `line`),
+	INDEX `opcode` (`opcode`),
+	CONSTRAINT `opcode` FOREIGN KEY (`opcode`) REFERENCES `op_code` (`code`) ON UPDATE CASCADE,
+	CONSTRAINT `vout_instruction` FOREIGN KEY (`txid`, `voutid`) REFERENCES `vout` (`txid`, `voutid`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+
+
+CREATE TABLE `vin` (
+	`txid` VARCHAR(64) NOT NULL,
+	`vinid` INT NOT NULL,
+	`txidout` VARCHAR(64) NOT NULL,
+	`voutid` INT NOT NULL,
+	`sequence` INT NULL,
+	PRIMARY KEY (`txid`, `vinid`),
+	INDEX `txidout_voutid` (`txidout`, `voutid`),
+	CONSTRAINT `vout_vin` FOREIGN KEY (`txidout`, `voutid`) REFERENCES `vout` (`txid`, `voutid`) ON UPDATE RESTRICT,
+	CONSTRAINT `transaction_vin` FOREIGN KEY (`txid`) REFERENCES `transaction` (`txid`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+
+CREATE TABLE `input_script_instruction` (
+	`txid` VARCHAR(64) NOT NULL,
+	`vinid` INT NOT NULL,
+	`line` INT NOT NULL,
+	`opcode` INT NULL DEFAULT NULL,
+	`value` INT NULL DEFAULT NULL,
+	PRIMARY KEY (`txid`, `vinid`, `line`),
+	CONSTRAINT `vin_script` FOREIGN KEY (`txid`, `vinid`) REFERENCES `vin` (`txid`, `vinid`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `vin_opcode` FOREIGN KEY (`opcode`) REFERENCES `op_code` (`code`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+CREATE TABLE `txinwitness` (
+	`txid` VARCHAR(64) NOT NULL,
+	`vinid` INT NOT NULL,
+	`witnessnr` INT NOT NULL,
+	`value` VARCHAR(255) NULL,
+	PRIMARY KEY (`txid`, `vinid`, `witnessnr`),
+	CONSTRAINT `vin_witness` FOREIGN KEY (`txid`, `vinid`) REFERENCES `vin` (`txid`, `vinid`) ON UPDATE CASCADE ON DELETE CASCADE
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
