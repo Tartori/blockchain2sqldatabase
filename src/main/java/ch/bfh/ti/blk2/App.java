e5ccdff245db99bf94883319d4ebc62d57ea0f26
Julian <julianstampfli4@gmail.com>
2018-05-07 13:38:55 +0200

    first version of blockchain loader

--- src/main/java/ch/bfh/ti/blk2/App.java
+++ src/main/java/ch/bfh/ti/blk2/App.java
@@ -1,13 +1,23 @@
 package ch.bfh.ti.blk2;
 
+import wf.bitcoin.javabitcoindrpcclient.BitcoinJSONRPCClient;
+import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient;
+
+import java.net.URL;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        try {
+            BlockchainLoader loader = new BlockchainLoader();
+            loader.load();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/ch/bfh/ti/blk2/BlockchainLoader.java
+++ src/main/java/ch/bfh/ti/blk2/BlockchainLoader.java
@@ -1,20 +1,47 @@
 package ch.bfh.ti.blk2;
 
+import wf.bitcoin.javabitcoindrpcclient.BitcoinJSONRPCClient;
 import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient;
 
-import java.sql.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.stream.Collectors;
 
 public class BlockchainLoader {
+    URL blockchainUrl;
+    BitcoinJSONRPCClient client;
 
-
-
-    public void saveBlockToDatabase(BitcoindRpcClient.Block block) throws SQLException, ClassNotFoundException {
-        Class.forName("org.mariadb.jdbc.Driver");
-        Connection con= DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/testnet3","testnet3","testnet3");
-        Statement d = con.createStatement();
-        d.execute("delete from testnet3.block;");
-        PreparedStatement s = InsertStatements.generateBlockStatement(con, block);
-        s.execute();
+    public BlockchainLoader() throws MalformedURLException {
+        blockchainUrl= new URL("http://user:password@127.0.0.1:18332");
+        client = new BitcoinJSONRPCClient(blockchainUrl);
     }
+
+
+    public void load(){
+
+        try {
+            DatabaseConnector connector = new DatabaseConnector();
+            for(OpCode code: OpCode.values()){
+                connector.saveOpCodeToDatabase(code);
+            }
+            for(int i=client.getBlockCount()-20;i<=client.getBlockCount();i++) {
+                BitcoindRpcClient.Block blk = client.getBlock(i);
+                connector.saveBlockToDatabase(blk);
+                for(BitcoindRpcClient.RawTransaction tx : blk.tx().stream().map(x->client.decodeRawTransaction(client.getRawTransactionHex(x))).collect(Collectors.toList())){
+                    connector.saveTransactionToDatabase(tx, blk.hash());
+                    for(BitcoindRpcClient.RawTransaction.Out vout: tx.vOut()){
+                        connector.saveVOutToDatabase(vout, tx.txId());
+                    }
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
