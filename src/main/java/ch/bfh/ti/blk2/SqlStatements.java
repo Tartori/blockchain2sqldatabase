1491a77a6b8205349c012d019d431455c6871b59
Julian <julianstampfli4@gmail.com>
2018-05-18 20:17:47 +0200

    added select query

--- src/main/java/ch/bfh/ti/blk2/SqlStatements.java
+++ src/main/java/ch/bfh/ti/blk2/SqlStatements.java
@@ -7,7 +7,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.SQLType;
 
-public class SQLQuerries {
+public class SqlStatements {
 
     public static final String BLOCK_INSERT_STATEMENT = "INSERT INTO `block` (`block_hash`, `prev_block_hash`, `height`, `size`, `version`, `merkleroot`, `time`, `difficulty`, `chainwork`, `nonce`) VALUES (?,?,?,?,?,?,?,?,?,?)";
     public static final String TRANSACTION_INSERT_STATEMENT = "INSERT INTO `transaction` (`txid`, `block_hash`, `txhash`, `version`, `size`, `locktime`) VALUES (?,?,?,?,?,?)";
@@ -17,4 +17,6 @@ public class SQLQuerries {
     public static final String VIN_INSTRUCTION_INSERT_STATEMENT  = "INSERT INTO `input_script_instruction` (`txid`, `vinid`, `line`, `opcode`, `value`)  VALUES (?,?,?,?,?)";
     public static final String VIN_WITNESS_INSERT_STATEMENT = "INSERT INTO `txinwitness` (`txid`, `vinid`, `witnessnr`, `value`) VALUES (?,?,?,?)";
     public static final String OP_CODE_INSERT_STATEMENT = "INSERT INTO `op_code` (`code`, `name`) VALUES (?,?)";
+
+    public static final String MAX_BLOCK_SELECT = "SELECT max(height) FROM `block`;";
 }
